f7fadbc26672428ca9749435dfef71ef9de03b0c
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2025-02-18 19:10:28 -0700

    made changes

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ mkdir finished
 mkdir finished/boot
 mkdir finished/boot/overlays
 
-sudo cp arch/arm/boot/zImage /finished/boot/kernel.img
-sudo cp arch/arm/boot/dts/*.dtb /finished/boot/
-sudo cp arch/arm/boot/dts/overlays/*.dtb* /finished/boot/overlays/
-sudo cp arch/arm/boot/dts/overlays/README /finished/boot/overlays/
+sudo cp arch/arm64/boot/zImage /finished/boot/kernel.img
+sudo cp arch/arm64/boot/dts/*.dtb /finished/boot/
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* /finished/boot/overlays/
+sudo cp arch/arm64/boot/dts/overlays/README /finished/boot/overlays/
 
 wget -o /finished/boot/bootcode.bin https://github.com/raspberrypi/firmware/blob/master/boot/bootcode.bin
 
@@ -15,4 +15,4 @@ wget -o /finished/boot/fixup.dat https://github.com/raspberrypi/firmware/blob/ma
 wget -o /finished/boot/fixup4.dat https://github.com/raspberrypi/firmware/blob/master/boot/fixup4.dat
 
 wget -o /finished/boot/start.elf https://github.com/raspberrypi/firmware/blob/master/boot/start.elf
-wget -o /finished/boot/start4.elf https://github.com/raspberrypi/firmware/blob/master/boot/start4.ef
+wget -o /finished/boot/start4.elf https://github.com/raspberrypi/firmware/blob/master/boot/start4.elf
