adb73790ee7f8e6fe95c107d43ef384d55d8eaaa
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2025-02-18 19:04:51 -0700

    made changes

--- build.sh
+++ build.sh
@@ -10,3 +10,9 @@ sudo cp arch/arm/boot/dts/overlays/*.dtb* /finished/boot/overlays/
 sudo cp arch/arm/boot/dts/overlays/README /finished/boot/overlays/
 
 wget -o /finished/boot/bootcode.bin https://github.com/raspberrypi/firmware/blob/master/boot/bootcode.bin
+
+wget -o /finished/boot/fixup.dat https://github.com/raspberrypi/firmware/blob/master/boot/fixup.dat
+wget -o /finished/boot/fixup4.dat https://github.com/raspberrypi/firmware/blob/master/boot/fixup4.dat
+
+wget -o /finished/boot/start.elf https://github.com/raspberrypi/firmware/blob/master/boot/start.elf
+wget -o /finished/boot/start4.elf https://github.com/raspberrypi/firmware/blob/master/boot/start4.ef
